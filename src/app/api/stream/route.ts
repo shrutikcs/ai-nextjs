604a1e02dc05076196a0ff7937c934e3761f0523
Shrutik Meshram <vlaveanticheat@gmail.com>
2026-03-17 17:28:32 +0530

    track usage

--- src/app/api/stream/route.ts
+++ src/app/api/stream/route.ts
@@ -9,6 +9,15 @@ export async function POST(req: Request) {
       model: groq("llama-3.3-70b-versatile"),
       prompt,
     });
+
+    result.usage.then((usage)=>{
+      console.log({
+        inputTokens: usage.inputTokenDetails,
+        outputTokens: usage.outputTokenDetails,
+        totalTokens: usage.totalTokens
+      })
+    })
+
     return result.toUIMessageStreamResponse();
   } catch (error) {
     console.error("error generating text:", error);
