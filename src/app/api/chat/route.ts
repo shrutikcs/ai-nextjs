02f85fc2969070530b7f5132866156b4a2aa0760
Shrutik Meshram <vlaveanticheat@gmail.com>
2026-03-17 20:32:46 +0530

    add system prompt

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -3,27 +3,30 @@ import { convertToModelMessages, streamText, UIMessage } from "ai"
 
 export async function POST(req: Request) {
   try {
-  const { messages } : { messages: UIMessage[]} = await req.json()
+    const { messages }: { messages: UIMessage[] } = await req.json()
 
-  const result = streamText({
-    model: groq("llama-3.3-70b-versatile"),
-    messages: await convertToModelMessages(messages)
-  })
-
-  result.usage.then((usage)=>{
-    console.log({
-      messageCount: messages.length,
-      inputToken: usage.inputTokenDetails,
-      outputToken: usage.outputTokenDetails,
-      totalToken: usage.totalTokens,
+    const result = streamText({
+      model: groq("llama-3.3-70b-versatile"),
+      messages: [{
+        role: "system",
+        content: "you are direct. answer using KISS principle in under 3 lines."
+      }, ...await convertToModelMessages(messages)]
     })
-  })
-  
-  return result.toUIMessageStreamResponse()
-  // return result.toTextStreamResponse()
-    
+
+    result.usage.then((usage) => {
+      console.log({
+        messageCount: messages.length,
+        inputToken: usage.inputTokenDetails,
+        outputToken: usage.outputTokenDetails,
+        totalToken: usage.totalTokens,
+      })
+    })
+
+    return result.toUIMessageStreamResponse()
+    // return result.toTextStreamResponse()
+
   } catch (error) {
     console.error("error streaming chat completion", error)
-    return new Response("failed to stream chat completion", {status: 500})
+    return new Response("failed to stream chat completion", { status: 500 })
   }
 }
